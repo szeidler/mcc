e74a4faf3e6eae9dfd4acedbc19a9b6a7ba63ec2
Marton Juhasz <m@juhaszmarton.hu>
2012-03-26 22:09:40 +0200

    functions and variables renamed - Rename 2.

--- node--mcc.tpl.php
+++ node--mcc.tpl.php
@@ -1,9 +1,8 @@
 <?php
-// $Id: node.tpl.php,v 1.2 2010/12/01 00:18:15 webchick Exp $
 
 /**
  * @file
- * MailChimp Campaign module node template for campaigns.
+ * MailChimp Campaigns module node template for campaigns.
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
